40c964545ce17cd3e707866df793ee6cec8fdc96
Rashmi <rashmi.raviprasad@gmail.com>
2017-05-19 15:58:51 -0700

    more changes to get_ra_dec

--- apps/get_ra_dec/get_ra_dec.py
+++ apps/get_ra_dec/get_ra_dec.py
@@ -5,6 +5,28 @@ from lab_analysis.libs.geometry import coordinates as crd
 from datetime import datetime
 np.set_printoptions(edgeitems = 50)
 
+"""
+class ScanParameters:
+	def __init__(self, az_0, el_0, a_thr, e_rng, e_stp, dt_scn):
+		#all of these values must be floats
+		self.az_0 = az_0
+		self.el_0 = el_0
+		self.a_thr = a_thr
+		self.e_rng = e_rng
+		self.e_stp = e_stp
+		self.dt_scn = dt_scn
+
+	def get_ra_dec(self, obs):
+		#obs is an ephem.Observer() object
+		[... all the code below ...]
+		return ra_data, dec_data
+
+if __name__ == '__main__':
+	get_ra_dec(self, obs)   [<---is this correct?]
+
+
+
+"""
 #parameters (either user input or default)
 response = raw_input('For default values, enter d. For custom values, enter c. ')
 if response == 'd':
@@ -12,7 +34,7 @@ if response == 'd':
 	el_0 = 50
 	a_thr = 100
 	e_rng = 100
-	e_stp = 1
+	e_stp = 5
 	dt_scn = 1
 	print('Default values:\n  az_0 = 50 deg \n  el_0 = 50 deg \n  az throw = 100 deg \n  el range = 100 deg \n  el step size = 1 deg \n  scan speed = 1 deg/sec')
 if response == 'c':
@@ -24,10 +46,13 @@ if response == 'c':
 	dt_scn = float(input('Enter the scan speed in degrees/sec: '))
 
 
+
+#if using Los Angeles
+#la = ephem.city('Los Angeles')
+#if using custom location:
 usc = ephem.Observer()
-usc.date = datetime.utcnow()
-usc.lon = -118.286926
-usc.lat = 34.019579
+usc.lon = '-118.286926'
+usc.lat = '34.019579'
 
 
 #defining a few useful values
@@ -52,23 +77,34 @@ dec_data = np.empty(int(t_end/dt))
 for i in range (0, n_stp):
 	if i == 0:
 	#first step of the scan: makes sure we're not appending to an empty array
-		ra_min, dec_min = crd.hor_to_eq(a_min, e_min, usc.lat(), usc.sidereal_time())
-		ra_max, dec_max = crd.hor_to_eq(a_max, e_max, usc.lat(), usc.sidereal_time())
-		dec_stp = (dec_max - dec_min)/n_stp - 1
-		ra_data = np.linspace(ra_min, ra_max, (ra_max - ra_min)/dt)
-		dec_data = np.repeat(dec_min, (ra_max - ra_min)/dt)
+		#la.date = datetime.utcnow()
+		#ra_min, dec_min = crd.hor_to_eq(a_min, e_min, la.lat, la.sidereal_time())
+		#ra_max, dec_max = crd.hor_to_eq(a_max, e_max, la.lat, la.sidereal_time())
+		usc.date = datetime.utcnow()
+		ra_min, dec_min = crd.hor_to_eq(a_min, e_min, float(usc.lat), usc.sidereal_time())
+		ra_max, dec_max = crd.hor_to_eq(a_max, e_max, float(usc.lat), usc.sidereal_time())
+		#print ra_min, ra_max
+		#print dec_min, dec_max, '\n'
+		dec_stp = abs(dec_max - dec_min)/(n_stp - 1)
+		ra_data = np.linspace(ra_min, ra_max, abs(ra_max - ra_min)/dt)
+		dec_data = np.repeat(dec_min, abs(ra_max - ra_min)/dt)
 		ra_data = np.append(ra_data, np.repeat(ra_max, dec_stp/dt))
 		dec_data = np.append(dec_data, np.linspace(dec_min, dec_min + dec_stp, dec_stp/dt))
 		flag = 1
 		continue
 	
 	if flag == 1:
-		ra_min, dec_min = crd.hor_to_eq(a_min, e_min, usc.lat(), usc.sidereal_time())
-		ra_max, dec_max = crd.hor_to_eq(a_max, e_max, usc.lat(), usc.sidereal_time())
-		dec_stp = (dec_max - dec_min)/n_stp - 1
-   
-                ra_data = np.append(ra_data, np.linspace(ra_max, ra_min, (ra_max - ra_min)/dt))
-                dec_data = np.append(dec_data, np.repeat(dec_min + i * dec_stp, (ra_max - ra_min)/dt))
+		#la.date = datetime.utcnow()
+		#ra_min, dec_min = crd.hor_to_eq(a_min, e_min, la.lat, la.sidereal_time())
+		#ra_max, dec_max = crd.hor_to_eq(a_max, e_max, la.lat, la.sidereal_time())
+		usc.date = datetime.utcnow()
+		ra_min, dec_min = crd.hor_to_eq(a_min, e_min, float(usc.lat), usc.sidereal_time())
+		ra_max, dec_max = crd.hor_to_eq(a_max, e_max, float(usc.lat), usc.sidereal_time())
+		#print ra_min, ra_max
+		#print dec_min, dec_max, '\n'
+		dec_stp = abs(dec_max - dec_min)/(n_stp - 1)   
+                ra_data = np.append(ra_data, np.linspace(ra_max, ra_min, abs(ra_max - ra_min)/dt))
+                dec_data = np.append(dec_data, np.repeat(dec_min + i * dec_stp, abs(ra_max - ra_min)/dt))
 		if (e_min + (i+1) * e_stp) > e_max:
 		#stops if this step will exceed e_max
 			break
@@ -79,12 +115,17 @@ for i in range (0, n_stp):
                         continue
 
 	else:
-		ra_min, dec_min = crd.hor_to_eq(a_min, e_min, usc.lat(), usc.sidereal_time())
-		ra_max, dec_max = crd.hor_to_eq(a_max, e_max, usc.lat(), usc.sidereal_time())
-		dec_stp = (dec_max - dec_min)/n_stp - 1
-
-		ra_data = np.append(ra_data, np.linspace(ra_min, ra_max, (ra_max - ra_min)/dt))
-		dec_data = np.append(dec_data, np.repeat(dec_min + i * dec_stp, (ra_max - ra_min)/dt))
+		#la.date = datetime.utcnow()
+		#ra_min, dec_min = crd.hor_to_eq(a_min, e_min, la.lat, la.sidereal_time())
+		#ra_max, dec_max = crd.hor_to_eq(a_max, e_max, la.lat, la.sidereal_time())
+		usc.date = datetime.utcnow()
+		ra_min, dec_min = crd.hor_to_eq(a_min, e_min, float(usc.lat), usc.sidereal_time())
+		ra_max, dec_max = crd.hor_to_eq(a_max, e_max, float(usc.lat), usc.sidereal_time())
+		#print ra_min, ra_max
+		#print dec_min, dec_max, '\n'
+		dec_stp = abs(dec_max - dec_min)/(n_stp - 1)
+		ra_data = np.append(ra_data, np.linspace(ra_min, ra_max, abs(ra_max - ra_min)/dt))
+		dec_data = np.append(dec_data, np.repeat(dec_min + i * dec_stp, abs(ra_max - ra_min)/dt))
 		if (e_min + (i+1) * e_stp) > e_max:
 		#stops if this step will exceed e_max
 			break
@@ -94,11 +135,6 @@ for i in range (0, n_stp):
 			flag = 1
 			continue
 
-
-
-print ra_data
-
-print dec_data
 
 
 plt.plot(ra_data, dec_data, 'b')
