abdbf8de2488b44812f82f31bc499ea8c3443b05
Rashmi <rashmi.raviprasad@gmail.com>
2017-05-18 17:38:26 -0700

    changes to get_ra_dec.py

--- apps/get_ra_dec/get_ra_dec.py
+++ apps/get_ra_dec/get_ra_dec.py
@@ -24,6 +24,12 @@ if response == 'c':
 	dt_scn = float(input('Enter the scan speed in degrees/sec: '))
 
 
+usc = ephem.Observer()
+usc.date = datetime.utcnow()
+usc.lon = -118.286926
+usc.lat = 34.019579
+
+
 #defining a few useful values
 n_stp = int(e_rng/e_stp) + 1 #number of steps
 l = a_thr*(n_stp) + e_rng*2 #total length of path of scan
@@ -38,57 +44,63 @@ flag = 0 #flag helps to establish zig zag pattern
 
 #arrays that the data will be in
 t = np.linspace(0., t_end, t_end/dt)
-az_data = np.empty(int(t_end/dt))
-el_data = np.empty(int(t_end/dt))
+ra_data = np.empty(int(t_end/dt))
+dec_data = np.empty(int(t_end/dt))
 
 
 #scan sequence
 for i in range (0, n_stp):
 	if i == 0:
 	#first step of the scan: makes sure we're not appending to an empty array
-		az_data = np.linspace(a_min, a_max, a_thr/dt)
-		el_data = np.repeat(e_min, a_thr/dt)
-		az_data = np.append(az_data, np.repeat(a_max, e_stp/dt))
-		el_data = np.append(el_data, np.linspace(e_min, e_min + e_stp, e_stp/dt))
+		ra_min, dec_min = crd.hor_to_eq(a_min, e_min, usc.lat(), usc.sidereal_time())
+		ra_max, dec_max = crd.hor_to_eq(a_max, e_max, usc.lat(), usc.sidereal_time())
+		dec_stp = (dec_max - dec_min)/n_stp - 1
+		ra_data = np.linspace(ra_min, ra_max, (ra_max - ra_min)/dt)
+		dec_data = np.repeat(dec_min, (ra_max - ra_min)/dt)
+		ra_data = np.append(ra_data, np.repeat(ra_max, dec_stp/dt))
+		dec_data = np.append(dec_data, np.linspace(dec_min, dec_min + dec_stp, dec_stp/dt))
 		flag = 1
 		continue
 	
-	if flag == 1:   
-                az_data = np.append(az_data, np.linspace(a_max, a_min, a_thr/dt))
-                el_data = np.append(el_data, np.repeat(e_min + i * e_stp, a_thr/dt))
+	if flag == 1:
+		ra_min, dec_min = crd.hor_to_eq(a_min, e_min, usc.lat(), usc.sidereal_time())
+		ra_max, dec_max = crd.hor_to_eq(a_max, e_max, usc.lat(), usc.sidereal_time())
+		dec_stp = (dec_max - dec_min)/n_stp - 1
+   
+                ra_data = np.append(ra_data, np.linspace(ra_max, ra_min, (ra_max - ra_min)/dt))
+                dec_data = np.append(dec_data, np.repeat(dec_min + i * dec_stp, (ra_max - ra_min)/dt))
 		if (e_min + (i+1) * e_stp) > e_max:
 		#stops if this step will exceed e_max
 			break
                 else:           
-                        az_data = np.append(az_data, np.repeat(a_min, e_stp/dt))
-                        el_data = np.append(el_data, np.linspace(e_min + i * e_stp, e_min + (i+1) * e_stp, e_stp/dt))
+                        ra_data = np.append(ra_data, np.repeat(ra_min, dec_stp/dt))
+                        dec_data = np.append(dec_data, np.linspace(dec_min + i * dec_stp, dec_min + (i+1) * dec_stp, dec_stp/dt))
                         flag = 0
                         continue
 
 	else:
-		az_data = np.append(az_data, np.linspace(a_min, a_max, a_thr/dt))
-		el_data = np.append(el_data, np.repeat(e_min + i * e_stp, a_thr/dt))
+		ra_min, dec_min = crd.hor_to_eq(a_min, e_min, usc.lat(), usc.sidereal_time())
+		ra_max, dec_max = crd.hor_to_eq(a_max, e_max, usc.lat(), usc.sidereal_time())
+		dec_stp = (dec_max - dec_min)/n_stp - 1
+
+		ra_data = np.append(ra_data, np.linspace(ra_min, ra_max, (ra_max - ra_min)/dt))
+		dec_data = np.append(dec_data, np.repeat(dec_min + i * dec_stp, (ra_max - ra_min)/dt))
 		if (e_min + (i+1) * e_stp) > e_max:
 		#stops if this step will exceed e_max
 			break
 		else:
-			az_data = np.append(az_data, np.repeat(a_max, e_stp/dt))
-			el_data = np.append(el_data, np.linspace(e_min + i * e_stp, e_min + (i+1) * e_stp, e_stp/dt))
+			ra_data = np.append(ra_data, np.repeat(ra_max, dec_stp/dt))
+			dec_data = np.append(dec_data, np.linspace(dec_min + i * dec_stp, dec_min + (i+1) * dec_stp, dec_stp/dt))
 			flag = 1
 			continue
 
 
-usc = ephem.Observer()
-usc.date = datetime.utcnow()
-usc.lon = -118.286926
-usc.lat = 34.019579
 
-ra, dec = crd.hor_to_eq(az_data, el_data, usc.lat, usc.sidereal_time())
+print ra_data
 
-print ra
-
-print dec
+print dec_data
 
 
-
+plt.plot(ra_data, dec_data, 'b')
+plt.show()
 
