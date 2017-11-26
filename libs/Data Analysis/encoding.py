c0ca46d6a2b28432a04f2da987ea067745788292
Jason Williams <30938238+williamscommajason@users.noreply.github.com>
2017-11-26 10:30:18 -0800

    Beginnings of Huffman code

--- libs/Data Analysis/encoding.py
+++ libs/Data Analysis/encoding.py
@@ -10,6 +10,7 @@ class Tree:
         self.left = None
         self.right = None
 
+    """Apparently Python 3 no longer supports __cmp__..."""
     def __add__(self, other):
         if isinstance(other, Tree):
             return Tree(self.freq + other.freq, None, self, other)
@@ -44,8 +45,6 @@ class Tree:
         else:
             return NotImplemented
 
-    def order(self,other):
-        if self.
 
 
 class Huff:
@@ -53,21 +52,16 @@ class Huff:
     def __init__(self, path):
         self.path = path
         self.heap = []
-        #need some stuff here for decoding
+        self.codes = {}
 
-    def split(self, input):
-        """ Splits our input into seperate strings by the space delimiter"""
-        if input is not None:
-            input = input.split()
-
-        return input
 
     def dict(self, input):
         """Create our frequency dictionary in the form: symbol: frequency; symbol frequency...."""
         frequency = {}
+
         if input is not None:
-            for i in len(input):
-                for j in input[i]:
+            for i in range(len(input)):
+                for j in input:
                     if j in frequency:
                         frequency[j] = frequency[j] + 1
                     else:
@@ -75,12 +69,53 @@ class Huff:
 
             return frequency
 
-    def base_heap(self, freq):
-        """Defines the base heap (set of bottom nodes) bases on the frequency dictionary. We want to define a particular
-        order for our base heap, and thus sort the fre"""
-        for i in freq:
-            node = Tree(i, freq(i))
+
+    def pqueue(self, freq):
+        for key in freq:
+            self.heap.append(Tree(key, freq[key]))
+
+        heapq.heapify(self.heap)
+
+        while len(self.heap) > 1:
+            left = heapq.heappop(self.heap)
+            right = heapq.heappop(self.heap)
+            node = Tree(None, right.freq + left.freq)
+            node.left = left
+            node.right = right
+
             heapq.heappush(self.heap, node)
+
+    def pre_encode(self, s, root):
+
+        if root.char:
+            if not s:
+                self.codes[root.char] = "0"
+            else:
+                self.codes[root.char] = s
+
+        else:
+
+            self.pre_encode(s + "0", root.left)
+            self.pre_encode(s + "1", root.right)
+
+    def encode(self):
+        root = heapq.heappop(self.heap)
+        s = ""
+        self.pre_encode(s, root)
+
+    def compress(self):
+
+        inp = self.path
+        frequency = self.dict(inp)
+        self.pqueue(frequency)
+        self.encode()
+
+        return self.codes, "".join([self.codes[a] for a in inp])
+
+
+
+
+
 
 
 
