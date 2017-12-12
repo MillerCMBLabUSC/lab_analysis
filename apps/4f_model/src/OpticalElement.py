Fixed parsing optical chain file, and unit conversion problem

apps/4f_model/src/OpticalElement.py
@@ -15,24 +15,25 @@ pW = 1.e12 # W -> pW
 
 eps0 = 8.85e-12 #Vacuum Permitivity
 rho=2.417e-8 #Resistivity of the mirror
+c = 299792458.0 #Speed of light [m/s]
 
 
 class OpticalElement:
     """ Optical Element
         *****
         Optional Parmeters:
-            :Thick: Thickness [mm]
+            :Thick: Thickness [m]
             :Index: Index of refraction
-            :LossTan: Loss Tangent
-            :Absorb: Absorption Coefficient
+            :LossTan: Loss Tangent 
+            :Absorb: Absorption Coefficient 
             :Refl: Reflection coefficient
             :Spill: Spillover Fraction
-            :ScattFrac: Scattered Fraction
-            :ScattTemp: Scattered Temperature
-            :SpillTemp: Spillover Temperature
-            :IP: IP value
+            :ScattFrac: Scattered Fraction 
+            :ScattTemp: Scattered Temperature [K]
+            :SpillTemp: Spillover Temperature [K]
+            :IP: IP value 
             :PolAbs: Polarized Absorption Coefficient
-            :Chi: Incident angle of Mirror
+            :Chi: Incident angle of Mirror [rad]
             :Freqs: Frequency Array for 
             :EffCurve: Efficiency Function (Corresponding to frequency array)
             :IPCurve: IP Function
@@ -44,28 +45,12 @@ class OpticalElement:
         self.temp = temp
         
         #Sets Default Optical Element Parameters
-        self.params = {"Thick": 0,      "Index": 0, "LossTan": 0, "Absorb": 0, \
+        self.params = {"Thick": 0,      "Index": 1.0, "LossTan": 0, "Absorb": 0, \
                        "Absorb": 0,     "Spill": 0, "SpillTemp": 0, "Refl": 0, "ScattFrac": 0, \
                        "ScattTemp": 0,  "IP": 0, "PolAbs": 0,  "Chi": 0, \
                        "Freqs": None,   "EffCurve": None, "IPCurve": None, "EmisCurve": None, "PolEmisCurve": None};
         
-        #Sets input parameters                       
-        for k in params.keys():
-            p = params[k]
-            if type(p) !=  np.string_:
-                self.params[k]= p
-            else:
-
-                try:
-                    #Converts to float if input is a string
-                    v = eval(params[k])
-                    if type(v) == list:
-                        self.params[k] = v[self.det.bid - 1]
-                    else:
-                        self.params[k] = v
-                except:
-                    if params[k] == "NA":
-                        self.params[k] = 0
+        self.updateParams(params)
 
     def updateParams(self, modifiedParams):
         self.params.update(modifiedParams)
@@ -80,7 +65,11 @@ class OpticalElement:
     #Absorption coefficient
     def Absorb(self, freq):
         if self.params["LossTan"] != 0:
-            return th.dielectricLoss(self.params["LossTan"], self.params["Thick"], self.params["Index"], freq)
+            # Calculates absorption from Loss Tangent
+            lamb = c / ( freq * self.params["Index"] )
+            alpha = 2 * np.pi * self.params["Index"] * self.params["LossTan"] / lamb
+            ab = 1.0 - np.exp(-alpha * self.params["Thick"])
+            return ab
         else:
             return self.params["Absorb"]
     
@@ -100,15 +89,16 @@ class OpticalElement:
     def Emis(self, freq):
         if self.params["EmisCurve"] is not None:
             return np.interp(freq, self.params["Freqs"], self.params["EmisCurve"])
+        
         if self.name == "Atm":
             # Returns 1 because we are using Rayleigh Jeans temperature
             return 1
         
-        if self.params["Spill"] != 0:
-            spillEmis = self.params["Spill"] * th.powFrac(self.params["SpillTemp"], self.temp, self.det.flo, self.det.fhi)
-        else:
-            spillEmis = 0
-            
+        # Gets extra emissivity due to spillover 
+        powFrac =  th.weightedSpec(freq, self.params["SpillTemp"], 1.)/th.weightedSpec(freq, self.temp, 1.)
+        spillEmis = powFrac * self.params["Spill"]
+
+
         if self.name == "Aperture":
             return (1 - self.Eff(freq) + spillEmis)
         else:
@@ -125,8 +115,8 @@ class OpticalElement:
         if self.name == "HWP":
             ao = 8.7*10**(-5) * (freq/ GHz) + 3.1*10**(-7)*(freq/GHz)**2 + (3.0)*10**(-10) * (freq/GHz)**3 #1/cm
             ae = 1.47*10**(-7) * (freq/GHz)**(2.2) #1/cm
-            Eotrans =  np.exp(self.params["Thick"]/10.0 * ao * self.temp/300) 
-            Eetrans =  np.exp(self.params["Thick"]/10.0 * ae * self.temp/300)
+            Eotrans =  np.exp(self.params["Thick"]*100.0 * ao * self.temp/300) 
+            Eetrans =  np.exp(self.params["Thick"]*100.0 * ae * self.temp/300)
             
             pemis = (abs(Eetrans)**2 - abs(Eotrans)**2) / 2            
             return pemis
@@ -145,7 +135,6 @@ def loadAtm(atmFile, det):
     e = OpticalElement("Atm", det, atmTemp, {"Freqs": freqs, "EffCurve": trans, "EmisCurve": emis})
     return e
     
-    
     #Reads Rayleigh Jeans temperature from file and takes average
 #    tempF = interpolate.interp1d(freqs, temps, kind = "linear")
 #    x = np.linspace(det.flo, det.fhi, 400)
@@ -158,15 +147,34 @@ def loadAtm(atmFile, det):
 def loadOpticalChain(opticsFile,det, theta = np.deg2rad(15./2)):
     """Returns list of optical elements from opticalChain.txt file. """
     
-    data = np.loadtxt(opticsFile, dtype=np.str)
-    keys = data[0]
-
     elements = []
     
+    data = np.loadtxt(opticsFile, dtype=np.str)
+    keys = data[0]
+    
+    # Units of optical chain columns:
+    # [sring, K, mm, NA, e-4, e6 S/m, NA, NA, K, um, NA, NA, K]
+    conversions = [None, 1, 1e-3, 1, 1e-4, 1e6, 1, 1, 1, 1e-6, 1, 1, 1]
+    
     for line in data[1:]:
-        params = dict(zip(keys, line))
+        params = {}
+        #Parses opticalChain.txt values and converts to correct units
+        for (i, k) in enumerate(keys):
+            value = line[i]
+            if (value) == "NA":
+                continue #Uses default optical element value
+            try:
+                v = eval(value)
+                if type(v) == list:
+                    params[k] = v[det.bid - 1] * conversions[i]
+                else:
+                    params[k] = v * conversions[i]
+            except:
+                params[k] = value       
+                
         name = params["Element"]        
         
+        # Adds IP and polarized absorption calculated using tmm
         if name == "AluminaF":
             (ip, polAbs) = IPCalc.getFilterIP(det.band_center, det.fbw, theta)
             params.update({"IP": ip, "PolAbs": polAbs})
@@ -174,8 +182,8 @@ def loadOpticalChain(opticsFile,det, theta = np.deg2rad(15./2)):
             (ip, polAbs) = IPCalc.getWinIP(det.band_center, det.fbw, theta)
             params.update({"IP": ip, "PolAbs": polAbs})
 
-        e = OpticalElement(name, det, eval(params["Temp"]), params = params)
+        e = OpticalElement(name, det, params["Temp"], params = params)        
         elements.append(e)
-
+        
     return elements
 

apps/4f_model/src/Telescope.py
@@ -28,9 +28,7 @@ class Telescope:
         self.det = dt.Detector(channelFile, cameraFile, bandID)
         self.freqs = np.linspace(self.det.flo, self.det.fhi, 400) #Frequency array of the detector
         
-        """
-            Creating the Optical Chain
-        """
+        """Creating the Optical Chain"""
         self.elements = [] #List of optical elements
 
     
@@ -71,6 +69,7 @@ class Telescope:
         
         
     def propSpectrum(self):
+        """Stores incident unpolarized power on each element in UPspecs"""
         self.UPspecs = [np.zeros(len(self.freqs))]  #Unpolarized spectrum before each element
         
         for (i, el) in enumerate(self.elements):
@@ -100,14 +99,13 @@ class Telescope:
             self.a4 += e.Ip(self.det.band_center)
         
     def getA4(self):
+        """Gets A4 power at the detector in pW"""
         self.A4 = 0
         for (i, e) in enumerate(self.elements[:self.hwpIndex]):            
             ppEmitted = th.weightedSpec(self.freqs, e.temp, e.pEmis)
             ppTransmitted = map(e.Ip, self.freqs)*self.UPspecs[i]
             specAtDetector = (ppEmitted + ppTransmitted)* map(lambda x : self.cumEff(i, x), self.freqs)
             ppTotal = .5 * abs(th.powFromSpec(self.freqs, specAtDetector))
-#            if ppTotal!=0:
-#                print e.name, ppTotal*pW
             self.A4 += ppTotal
             
     

apps/4f_model/src/thermo.py
@@ -5,42 +5,42 @@ import matplotlib.pyplot as plt
 
 #Physical Constants
 #Everything is in MKS units
-#Planck constant [J/s]
-h = 6.6261e-34
-#Boltzmann constant [J/K]
-kB = 1.3806e-23
-#Speed of light [m/s]
-c = 299792458.0
-#Pi
-PI = 3.14159265
-#Vacuum Permitivity
-eps0 = 8.85e-12
-#Resistivity of the mirror
-rho=2.417e-8
+h = 6.6261e-34  #Planck constant [J/s]
+kB = 1.3806e-23 #Boltzmann constant [J/K]
+c = 299792458.0 #Speed of light [m/s]
+PI = np.pi      #Pi
+eps0 = 8.85e-12 #Vacuum Permitivity
+rho=2.417e-8    #Resistivity of the mirror
+Tcmb = 2.725    #CMB Temperatrure [K]
 
+#Conversions
 GHz = 10 ** 9
 
-Tcmb = 2.725
+
 
 
 #Calculates total black body power for a given temp and emis.
 def bbSpec(freq,temp,emis):
+    if temp==0:
+        return 0
+    
     occ = 1.0/(np.exp(h*freq/(temp*kB)) - 1)
-
-    if callable(emis):
-        e = emis(freq)
-    else:
-        e = emis
-
+    e = emis(freq) if  callable(emis) else emis
     return (2*e*h*freq**3)/(c**2)* occ
 
-#Calculates total black body power for a given temp and emis multiplied by the optical throughput.
 def weightedSpec(freq,temp,emis):
+    """Calculates the Black body spectrum for a given temp and emis weighted by AOmega"""
+    if temp == 0:
+        return 0
+    
     AOmega = (c/freq)**2
-    return (AOmega*bbSpec(freq,temp,emis))
+    occ = 1.0/(np.exp(h*freq/(temp*kB)) - 1)
+    e = emis(freq) if  callable(emis) else emis
+    return AOmega * 2 * e * h * freq**3  * occ/ c**2
+
 
 def bbPower(temp, emis, f1,f2):
-    power = .5*intg.quad(lambda x: weightedSpec(x,temp,emis), f1, f2)[0]
+    power = .5 * intg.quad(lambda x: weightedSpec(x,temp,emis), f1, f2)[0]
     return power
 
 def powFromSpec(freqs, spec):
@@ -48,7 +48,7 @@ def powFromSpec(freqs, spec):
 
 
 def spillEff(det):
-    """" Calculates Spillover Efficiency of detector"""
+    """" Calculates Efficiency of the Aperture"""
     D = det.pixSize
     F = det.f_num
     waistFact = det.waistFact
@@ -56,27 +56,12 @@ def spillEff(det):
     return 1. - np.exp((-np.power(np.pi,2)/2.)*np.power((D/(waistFact*F*(c/freq))),2))
 
 
-def powFrac(T1, T2, f1, f2):
-        if T1==0:
-            return 0
-        else: 
-            return bbPower(T1, 1.0, f1, f2)/bbPower(T2, 1.0, f1, f2)
-
-
-def getLambdaOptCoeff(chi):
-    geom = (1 / np.cos(chi) - np.cos(chi))
-    return - 2 * geom * np.sqrt(4 * PI * eps0 * rho )
-
 def getLambdaOpt(nu, chi):
     geom = (1 / np.cos(chi) - np.cos(chi))
     return - 2 * geom * np.sqrt(4 * PI * eps0 * rho * nu)
 
-def aniPowSpec(emissivity, freq, temp=None):
-        if temp == None:
-            temp = Tcmb
-
+def aniPowSpec(emissivity, freq, temp=Tcmb):
         occ = 1.0/(np.exp(h*freq/(temp*kB)) - 1)
-
         return ((h**2)/kB)*emissivity*(occ**2)*((freq**2)/(temp**2))*np.exp((h*freq)/(kB*temp))
 
 
@@ -88,49 +73,12 @@ def dPdT(elements, det):
     return intg.quad(lambda x: aniPowSpec(totalEff(x), x, Tcmb), det.flo, det.fhi)[0]
     
 
-#***** Public Methods *****
-
-def lamb(freq, index=None):
-    """Convert from from frequency [Hz] to wavelength [m]"""
-    if index == None:
-        index = 1.
-    
-    return c/(freq*index)
-
 def dielectricLoss( lossTan, thickness, index, freq, atmScatter=0):
     """Dielectric loss coefficient with thickness [m] and freq [Hz]"""
-    return 1.0 - np.exp((-2*PI*index*lossTan*thickness)/lamb(freq/GHz))
+    lamb = c / (freq/GHz * index)
+    return 1.0 - np.exp((-2*PI*index*lossTan*thickness)/lamb)
 
 
 if __name__=="__main__":
-    bc = 145 * GHz
-    fbw = .276
-    flo = bc * (1 - fbw / 2)
-    fhi = bc * (1 + fbw / 2)
-    T = Tcmb
-
-    
-    #Exact
-    occ = lambda nu :  1./(np.exp(h * nu / (T * kB)) - 1)
-    aniSpec = lambda nu :  2 * h**2 * nu **2 / (kB * T**2) * occ(nu)**2 * np.exp(h * nu / (kB * T)) 
-    factor1 = intg.quad(aniSpec, flo, fhi)[0]
-    
-    
-    
-    cumEff = .3638
-    
-    factor2 = 2 * kB * (fhi - flo)
-    print factor1 * pW
-    print factor2 * pW
-    print factor2 / factor1
-#    freqs = np.linspace(flo, fhi, 30)
-#    plt.plot(freqs, aniSpec(freqs) / (2 * kB))
-##    plt.plot(freqs, [2 * kB for f in freqs])
-#    plt.show()
-    
-
-    
-    
-    
-    
+    print bbSpec(145 * GHz, 100, 1)
     