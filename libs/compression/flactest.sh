363850efdf101c447b474627435fcf1086b9dbf0
Jason <mrratch4pres@gmail.com>
2018-01-05 05:18:39 -0500

    fixed compression ratio calculuation and makewav allows command line input

--- libs/compression/flactest.sh
+++ libs/compression/flactest.sh
@@ -1,30 +1,47 @@
 #!/bin/bash
 
-$(python3 makewav.py)
 
+$(python3 makewav.py $1)
+
+
+#get original binary file size and make flac files
+
+find *.npy \
+	| \
+	(
+	while read file
+	do 
+	og_file_size=$(stat -c%s "$file")
+	flac=$(flac "$file" --compression-level-8 --endian="little" --sign="signed" --channels="1" --bps="16" --sample-rate="100" -f &> /dev/null)
+
+	#find the newly created .flac file, and then calculate the difference
+
+	find *.flac \
+		| \
+		(
+		while read otherfile
+		do
+		#get meta data from files just in case
+		meta=$(metaflac --show-bps --show-channels --show-total-samples "$otherfile")
+		
+		#get file size
+		file_size=$(stat -c%s "$otherfile")
+		
+		#calculate the compression ratio and place the value in a text file
+		perc=$(bc <<< "scale=5; ($og_file_size - $file_size)/$og_file_size * 100")
+		echo "$perc" >> arrayfile.txt
+		rm $otherfile
+		done
+		) 
+	done
+	)
+
+#remove the initial binary file
 for file in *.npy
-
-do 
-
-og_file_size=$(stat -c%s "$file")
-
-flac=$(flac "$file" --compression-level-8 --endian="little" --sign="signed" --channels="1" --bps="16" --sample-rate="100" -f)
-
+	do
+	rm $file
 done
 
-for file in *.flac
+#clear the text file
 
-do 
-
-#get meta-data from files just in case
-meta=$(metaflac --show-bps --show-channels --show-total-samples "$file")
-
-#get file size
-file_size=$(stat -c%s "$file")
-
-perc=$(bc <<< "scale =2; ($og_file_size - $file_size)/$og_file_size * 100 ")
-echo "$perc %"
-
-done
-
-
+#> arrayfile.txt
--- libs/compression/makewav.py
+++ libs/compression/makewav.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 import numpy as np
 from lab_analysis.libs.noise import simulate
-from tempfile import TemporaryFile
+import sys
+
 
 alpha = 1.0
 white_noise_sigma = 1.0
-length_ts = 1000000
+length_ts = 100000
 f_knee = 2.0
 sample_rate = 100
+realizations = int(sys.argv[1])
 
-for i in range(20):
+for i in range(realizations):
 	j = i + 1
 	noise = simulate.simulate_noise(alpha, white_noise_sigma,length_ts, f_knee, sample_rate)
 	scalednoise = np.int16(noise/np.max(noise) * 32767)
